862b51e6714b6b042edef0cffa2decaa2ed47b15
Denis Li <denisli@mit.edu>
2017-01-01 18:00:49 -0500

    Added in the default onset detection implementation to a function

--- onset_detection.py
+++ onset_detection.py
@@ -1,8 +1,14 @@
-import stft
+import numpy as np
+import spectral_difference
 import peak_finding
 
-def onsetDetection(signal):
+def getOnsets(signal):
   '''
   Returns a list of times for which the onset of the notes are at.
   '''
-  
+  hopSize = 5
+  sd = spectral_difference.spectralDifference(signal, 40, hopSize)
+  thresholds = peak_finding.medianFilter(sd, 999, 0, 1)
+  peakLocs = peak_finding.findPeaks(sd, thresholds, 2000)
+  onsets = np.multiply(peakLocs, hopSize)
+  return onsets
