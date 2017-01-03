4fa179a1bd44ab6222c3f7daaaf0acaa56875654
Denis Li <denisli@mit.edu>
2017-01-02 21:00:35 -0500

    Reducing the median filter kernel size because it took up too much time (and did not seem to affect much)

--- onset_detection.py
+++ onset_detection.py
@@ -8,7 +8,7 @@ def getOnsets(signal):
   '''
   hopSize = 5
   sd = spectral_difference.spectralDifference(signal, 40, hopSize)
-  thresholds = peak_finding.medianFilter(sd, 999, 0, 1)
+  thresholds = peak_finding.medianFilter(sd, 99, 0, 1)
   peakLocs = peak_finding.findPeaks(sd, thresholds, 2000)
   onsets = np.multiply(peakLocs, hopSize)
   return onsets
